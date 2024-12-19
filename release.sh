fa44aa0cc48d840763df3d086376e45fc77a4cc2
Asqar Arslanov <asqarslanov@gmail.com>
2024-12-20 00:40:15 +0300

    style: format with shfmt

--- release.sh
+++ release.sh
@@ -12,44 +12,38 @@ names["GoogleDot-Green"]="Green cursor theme inspired on Google"
 rm -rf themes bin
 
 # Building Google XCursor binaries
-for key in "${!names[@]}";
-do
-    comment="${names[$key]}";
-    ctgen build.toml -p x11 -d "bitmaps/$key" -n "$key" -c "$comment" &
-    PID=$!
-    wait $PID
+for key in "${!names[@]}"; do
+  comment="${names[$key]}"
+  ctgen build.toml -p x11 -d "bitmaps/$key" -n "$key" -c "$comment" &
+  PID=$!
+  wait $PID
 done
 
-
 # Building Google Windows binaries
-for key in "${!names[@]}";
-do
-    comment="${names[$key]}";
-    ctgen build.toml -p windows -s 16 -d "bitmaps/$key" -n "$key-Small" -c "$comment" &
-    ctgen build.toml -p windows -s 24 -d "bitmaps/$key" -n "$key-Regular" -c "$comment" &
-    ctgen build.toml -p windows -s 32 -d "bitmaps/$key" -n "$key-Large" -c "$comment" &
-    ctgen build.toml -p windows -s 48 -d "bitmaps/$key" -n "$key-Extra-Large" -c "$comment" &
-    PID=$!
-    wait $PID
+for key in "${!names[@]}"; do
+  comment="${names[$key]}"
+  ctgen build.toml -p windows -s 16 -d "bitmaps/$key" -n "$key-Small" -c "$comment" &
+  ctgen build.toml -p windows -s 24 -d "bitmaps/$key" -n "$key-Regular" -c "$comment" &
+  ctgen build.toml -p windows -s 32 -d "bitmaps/$key" -n "$key-Large" -c "$comment" &
+  ctgen build.toml -p windows -s 48 -d "bitmaps/$key" -n "$key-Extra-Large" -c "$comment" &
+  PID=$!
+  wait $PID
 done
 
 # Compressing Binaries
 mkdir -p bin
 cd themes
 
-for key in "${!names[@]}";
-do
-    tar -czvf "../bin/${key}.tar.gz" "${key}" &
-    PID=$!
-    wait $PID
+for key in "${!names[@]}"; do
+  tar -czvf "../bin/${key}.tar.gz" "${key}" &
+  PID=$!
+  wait $PID
 done
 
-
-for key in "${!names[@]}";
-do
-    zip -rv "../bin/${key}-Windows.zip" "${key}-Small-Windows" "${key}-Regular-Windows" "${key}-Large-Windows" "${key}-Extra-Large-Windows" &
-    PID=$!
-    wait $PID
+for key in "${!names[@]}"; do
+  zip -rv "../bin/${key}-Windows.zip" "${key}-Small-Windows" "${key}-Regular-Windows" "${key}-Large-Windows" "${key}-Extra-Large-Windows" &
+  PID=$!
+  wait $PID
 done
 
 cd ..
